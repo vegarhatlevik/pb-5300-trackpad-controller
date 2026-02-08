ddc62d1e90b698ad7263f7c1668cd761b90dd78e
vegar <4639627+vegarhatlevik@users.noreply.github.com>
2026-02-08 20:11:53 +0200

    added arduino sketch for firmware

---
+++ firmware/trackpad-controller/trackpad-controller.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  for (uint8_t i=2; i<=13; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
